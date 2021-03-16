fce9ed9fc839a8af897c540c08ac1cb98baa59eb
sgertrud <sgertrud@strudent.21-school.ru>
2021-03-16 19:00:39 +0300

    div

--- 02/ex03/Fixed.cpp
+++ 02/ex03/Fixed.cpp
@@ -148,7 +148,7 @@ Fixed operator / (const Fixed &a, const Fixed &b)
 {
 	Fixed result;
 
-	result.setRawBits((a.getRawBits() << Fixed::getOffset()) / b.getRawBits());
+	result.setRawBits((a.getRawBits() / b.getRawBits())<< Fixed::getOffset());
 	return (result);
 }
 
