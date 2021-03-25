24897ad6e50e04548e0b4038ea3791525e441d70
sgertrud <sgertrud@strudent.21-school.ru>
2021-03-25 07:03:53 +0300

    asdasd

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -36,33 +36,5 @@ int main(void)
 				std::cout << "NO GOD DaMN NO" << std::endl;
 			}
 	}
-	{
-		int x[] = {10, 3, 5};
-		std::vector <int> a(x, x + 3);
-		for (int i = 0; i < 11; i++)
-			try
-			{
-				easyfind(a, i);
-				std::cout << i << " Is iN it" << std::endl;
-			}
-			catch(std::exception)
-			{
-				std::cout << "NO GOD DaMN NO" << std::endl;
-			}
-	}
-	{
-		int x[] = {10, 3, 5};
-		std::vector <int> a(x, x + 3);
-		for (int i = 0; i < 11; i++)
-			try
-			{
-				easyfind(a, i);
-				std::cout << i << " Is iN it" << std::endl;
-			}
-			catch(std::exception)
-			{
-				std::cout << "NO GOD DaMN NO" << std::endl;
-			}
-	}
 	return 0;
 }
